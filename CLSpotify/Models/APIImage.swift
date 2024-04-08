bc2b6fda72254741f922a61ec4393faf07c8fdf8
Kevin <chenglu7701@gmail.com>
2024-04-08 19:33:16 +0800

    Added APIImage.swift, NewReleasesResponse, get NewReleases API Response.

---
+++ CLSpotify/Models/APIImage.swift
@@ -0,0 +1,12 @@
+//
+//  UserImage.swift
+//  CLSpotify
+//
+//  Created by ChengLu on 2024/4/8.
+//
+
+import Foundation
+
+struct APIImage: Codable {
+    let url: String
+}
---
+++ CLSpotify/Models/NewReleasesResponse.swift
@@ -0,0 +1,8 @@
+//
+//  NewReleasesResponse.swift
+//  CLSpotify
+//
+//  Created by ChengLu on 2024/4/8.
+//
+
+import Foundation
