df62c00d9c6e2adb61aae4f29a6b31f0caa438cd
Kevin <chenglu7701@gmail.com>
2024-04-25 15:54:07 +0800

    Constructing the SearchViewController functionality method

--- CLSpotify/Controllers/Core/SearchViewController.swift
+++ CLSpotify/Controllers/Core/SearchViewController.swift
@@ -7,24 +7,37 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UISearchResultsUpdating {
+
+    let searchController: UISearchController = {
+        let vc = UISearchController(searchResultsController: SearchResultsViewController())
+        vc.searchBar.placeholder = "Songs, Artists, Albums"
+        vc.searchBar.searchBarStyle = .minimal
+        vc.definesPresentationContext = true
+        return vc
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
 
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - UISearchResultsUpdating
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let resultController = searchController.searchResultsController as? SearchResultsViewController,
+              let query = searchController.searchBar.text,
+        !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        // resultsController.update(with: results)
+        print(query)
+        // Perform search
+        
+        // APICaller
     }
-    */
-
 }
