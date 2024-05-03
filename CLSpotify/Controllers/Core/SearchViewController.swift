f3ad145210b638c3a0b7b4feac36ae3d765bae06
Kevin <chenglu7701@gmail.com>
2024-05-03 11:48:29 +0800

    Added TableVIew layout and delegate in SearchResultsViewController

--- CLSpotify/Controllers/Core/SearchViewController.swift
+++ CLSpotify/Controllers/Core/SearchViewController.swift
@@ -89,6 +89,7 @@ class SearchViewController: UIViewController, UISearchResultsUpdating, UISearchB
         !query.trimmingCharacters(in: .whitespaces).isEmpty else {
             return
         }
+        resultController.delegate = self
         
         APICaller.shared.search(with: query) { result in
             DispatchQueue.main.async {
@@ -110,6 +111,32 @@ class SearchViewController: UIViewController, UISearchResultsUpdating, UISearchB
         // resultsController.update(with: results)
 
     }
+}
+
+extension SearchViewController: SearchResultsViewControllerDelegate {
+    func didTapResult(_ result: SearchResult) {
+        switch result {
+        case .artist(let model):
+            break
+        case .album(let model):
+            let vc = AlbumViewController(album: model)
+            vc.navigationItem.largeTitleDisplayMode = .never
+            navigationController?.pushViewController(vc, animated: true)
+        case .track(let model):
+            break
+        case .playlist(let model):
+            let vc = PlaylistViewController(playlist: model)
+            vc.navigationItem.largeTitleDisplayMode = .never
+            navigationController?.pushViewController(vc, animated: true)
+
+        }
+    }
+    
+    func text123() {
+        print("textasefsafsewafaesf")
+    }
+
+    
 }
 
 extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSource {
